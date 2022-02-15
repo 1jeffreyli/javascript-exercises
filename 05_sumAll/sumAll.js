7d4366f109800545e12f12af02ebfe5093a186d6
Jeffrey Li <lijeffre@grinnell.edu>
2022-02-15 14:10:02 +0900

    Add two conditions to pass fifth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(a, b) {
     let sum = 0;
-    if ( a < 0 || b < 0){
+    if ( a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number"){
         return "ERROR";
     } else if (a < b) {
         for (let i=a; i <= b; i++){
