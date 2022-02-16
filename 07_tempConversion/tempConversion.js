25c3e46d8ac070c7e67ea370d0ec18a45d5c6fa2
Jeffrey Li <lijeffre@grinnell.edu>
2022-02-16 15:07:29 +0900

    Write code to pass one decimal place test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const ftoc = function(f) {
   let c = (f - 32) * (5 / 9);
-  return c;  
+  const roundedC = Math.round(c * 10) / 10;
+  return roundedC;
 };
 
 const ctof = function(c) {
   let f = c * (9 / 5) + 32;
-  return f;
+  const roundedF = Math.round(f * 10) / 10;
+  return roundedF;
 };
 
 // Do not edit below this line
