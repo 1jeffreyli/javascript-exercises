d4e126330ec75292b9d7615b37c738bf3a659c57
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-07 22:47:10 +0900

    Add add and subtract functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,16 @@
-const add = function() {
-	
+const add = function(a, b) {
+  const addition = a + b;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  const result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+  return arrSum;
 };
 
 const multiply = function() {
