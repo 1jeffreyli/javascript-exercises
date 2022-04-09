c54f19017891666619b2794c249c2a2c4dad0045
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-09 15:56:46 +0900

    Add fibonacci functionality

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let number = parseInt(num);
+    if (number < 0) {
+        return "OOPS";
+    }
+    let n1 = 0, n2 = 1, nextTerm;
+    for (let i = 1; i <= number; i++) {
+        // console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1;
 };
 
 // Do not edit below this line
