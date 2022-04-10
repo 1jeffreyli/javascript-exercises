4356fe8d827c7a330e4f1a29bce30535277e25d3
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-10 15:16:16 +0900

    Add solution for living persons

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,18 @@
-const findTheOldest = function(people) {
-    let oldest = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
-    return oldest[0];
-}
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+};
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
