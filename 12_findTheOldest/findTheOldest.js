9a4fad934f12e0f3b4647032f0d6f5a83bd5ec44
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-10 14:58:58 +0900

    Add first oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,7 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldest = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return oldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
